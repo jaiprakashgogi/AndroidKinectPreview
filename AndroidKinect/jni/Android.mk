0a6bd29ce8d63b8dea03c268e9b20df5b28168b4
Jai Prakash <jaiprakashgogi@gmail.com>
2015-12-20 22:59:17 -0500

    [Android][Kinect] Adding libusb and libfreenect

--- AndroidKinect/jni/Android.mk
+++ AndroidKinect/jni/Android.mk
@@ -1,6 +1,15 @@
-LOCAL_PATH := $(call my-dir)
+TOP_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
+include $(TOP_PATH)/libusb-1.0.18/android/jni/libusb.mk
+include $(TOP_PATH)/libfreenect/android/jni/libfreenect.mk
+
+include $(CLEAR_VARS)
+LOCAL_PATH := $(TOP_PATH)
+include /home/jprakash/jai/android/OpenCV-android-sdk/sdk/native/jni/OpenCV.mk
+
+LOCAL_SHARED_LIBRARIES += libusb1.0 libfreenect
+LOCAL_LDLIBS     += -llog -ldl
 
 LOCAL_MODULE    := androidkinect
 LOCAL_SRC_FILES := androidkinect.cpp
--- AndroidKinect/src/com/jaiprakashgogi/androidkinect/KinectActivity.java
+++ AndroidKinect/src/com/jaiprakashgogi/androidkinect/KinectActivity.java
@@ -1,5 +1,9 @@
 package com.jaiprakashgogi.androidkinect;
 
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
@@ -11,20 +15,38 @@ import android.widget.Button;
 import android.widget.TextView;
 
 public class KinectActivity extends Activity {
-	
+
 	private static final String TAG = "KinectActivity";
 	private TextView textView;
 	private Button button;
-	
+
+	private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
+		@Override
+		public void onManagerConnected(int status) {
+			switch (status) {
+			case LoaderCallbackInterface.SUCCESS: {
+				Log.i(TAG, "OpenCV loaded successfully");
+				// Load native library after(!) OpenCV initialization
+				System.loadLibrary("androidkinect");
+			}
+				break;
+			default: {
+				super.onManagerConnected(status);
+			}
+				break;
+			}
+		}
+	};
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_kinect);
-		
+
 		textView = (TextView) findViewById(R.id.tv);
 		button = (Button) findViewById(R.id.button);
 		button.setOnClickListener(new OnClickListener() {
-			
+
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
 				textView.setText(callnative());
@@ -37,6 +59,13 @@ public class KinectActivity extends Activity {
 		// TODO Auto-generated method stub
 		super.onResume();
 		Log.i(TAG, "onResume Function");
+		if (!OpenCVLoader.initDebug()) {
+			Log.d(TAG, "Internal OpenCV library not found. Using OpenCV Manager for initialization");
+			OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_0_0, this, mLoaderCallback);
+		} else {
+			Log.d(TAG, "OpenCV library found inside package. Using it!");
+			mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+		}
 	}
 
 	@Override
@@ -44,11 +73,11 @@ public class KinectActivity extends Activity {
 		// TODO Auto-generated method stub
 		super.onPause();
 	}
-	
+
 	public native String callnative();
-	
-    static {
-        System.loadLibrary("androidkinect");
-    }
-	
+
+/*	static {
+		System.loadLibrary("androidkinect");
+	}*/
+
 }
